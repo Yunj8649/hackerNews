958a6586cf8d2146bdae60b0f334f11555bbd54a
yunj <yunj8649@xingxingmobility.com>
2021-11-13 09:55:53 +0900

    mixins 사용으로 변경

--- app.ts
+++ app.ts
@@ -40,35 +40,76 @@ const store: Store = {
     feeds: [],
 }
 
+// 타겟 클래스로 제공되는 클래스에 베이스 클래스로 제공되는 n개의 기능들을 합성시킨다. 
+function applyApiMixins(targetClass: any, baseClasses: any[]): void{
+    baseClasses.forEach(baseClass => {
+        Object.getOwnPropertyNames(baseClass.prototype).forEach(name => {
+            const descriptor = Object.getOwnPropertyDescriptor(baseClass.prototype, name);
+
+            if (descriptor) {
+                Object.defineProperties(targetClass.prototype, name, descriptor);
+            }
+        })
+    });
+}
+
 class Api { 
-    url: string;
-    ajax: XMLHttpRequest;
-
-    // 초기값 생성
-    constructor(url: string) {
-        this.url = url;
-        this.ajax = new XMLHttpRequest();
-    }
-
-    protected getRequest<AjaxResponse>(): AjaxResponse {
-        this.ajax.open('GET', this.url, false);   // open의 메소드는 첫번째 string, 두번째는 url 문자열 세번째는 async: boolean (false: 동기로 가져옴)
-        this.ajax.send(); // 데이터를 가져옴
+    protected getRequest<AjaxResponse>(url: string): AjaxResponse {
+        const ajax = new XMLHttpRequest();
+        ajax.open('GET', url, false);   // open의 메소드는 첫번째 string, 두번째는 url 문자열 세번째는 async: boolean (false: 동기로 가져옴)
+        ajax.send(); // 데이터를 가져옴
     
-        return JSON.parse(this.ajax.response);
+        return JSON.parse(ajax.response);
     }
 }
 
-class NewsFeedApi extends Api {
+class NewsFeedApi {
     getData(): NewsFeed[] {
-        return this.getRequest<NewsFeed[]>();
+        return this.getRequest<NewsFeed[]>(NEWS_URL);
     }
 }
 
-class NewsFeedDetailApi extends Api {
-    getData(): NewsDetail {
-        return this.getRequest<NewsDetail>();
+class NewsDetailApi {
+    getData(id: string): NewsDetail {
+        return this.getRequest<NewsDetail>(CONTENT_URL.replace('@id', id));
     }
 }
+
+interface NewsFeedApi extends Api {};
+interface NewsDetailApi extends Api {};
+applyApiMixins(NewsFeedApi, [Api]);
+applyApiMixins(NewsDetailApi, [Api]);
+
+
+// class Api { 
+//     url: string;
+//     ajax: XMLHttpRequest;
+
+//     // 초기값 생성
+//     constructor(url: string) {
+//         this.url = url;
+//         this.ajax = new XMLHttpRequest();
+//     }
+
+//     protected getRequest<AjaxResponse>(): AjaxResponse {
+//         this.ajax.open('GET', this.url, false);   // open의 메소드는 첫번째 string, 두번째는 url 문자열 세번째는 async: boolean (false: 동기로 가져옴)
+//         this.ajax.send(); // 데이터를 가져옴
+    
+//         return JSON.parse(this.ajax.response);
+//     }
+// }
+
+// class NewsFeedApi extends Api {
+//     getData(): NewsFeed[] {
+//         return this.getRequest<NewsFeed[]>();
+//     }
+// }
+
+// class NewsFeedDetailApi extends Api {
+//     getData(): NewsDetail {
+//         return this.getRequest<NewsDetail>();
+//     }
+// }
 
 
 function getData<AjaxResponse>(url: string): AjaxResponse  {
@@ -95,7 +136,7 @@ function updateView(html: string): void {
 }
 
 function newsFeed(): void {
-    const api = new NewsFeedApi(NEWS_URL);
+    const api = new NewsFeedApi();
     let newsFeed: NewsFeed[] = store.feeds;
     const newsList = [];
     let template = `
@@ -158,8 +199,8 @@ function newsFeed(): void {
 
 function newsDetail(): void {
     const id = location.hash.substr(7);
-    const api = new NewsFeedDetailApi(CONTENT_URL.replace('@id', id));
-    const newsContent = api.getData();
+    const api = new NewsDetailApi();
+    const newsDetail: NewsDetail = api.getData(id);
 
     let template = `
         <div class="bg-gray-600 min-h-screen pb-8">
@@ -179,9 +220,9 @@ function newsDetail(): void {
         </div>
 
         <div class="h-full border rounded-xl bg-white m-6 p-4 ">
-            <h2>${newsContent.title}</h2>
+            <h2>${newsDetail.title}</h2>
             <div class="text-gray-400 h-20">
-            ${newsContent.content}
+            ${newsDetail.content}
             </div>
 
             {{__comments__}}
@@ -197,7 +238,7 @@ function newsDetail(): void {
         }
     }
     
-    updateView(template.replace('{{__comments__}}', makeComment(newsContent.comments)));
+    updateView(template.replace('{{__comments__}}', makeComment(newsDetail.comments)));
 }
 
 function makeComment(comments: NewsComment[]): string {
